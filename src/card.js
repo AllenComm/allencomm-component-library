3bde1f5e0a2ed1ab691949facf336c61cfca9266
Scott Zackrison <cszackrison@gmail.com>
2023-03-08 13:06:12 -0700

    switched default to box-sizing: border-box

--- src/card.js
+++ src/card.js
@@ -4,6 +4,9 @@ export default class Card extends HTMLElement {
 		this.attachShadow({ mode: 'open' });
 		this.shadowRoot.innerHTML = `
 			<style>
+				* {
+					box-sizing: border-box;
+				}
 				div.ac-card {
 					display: flex;
 					flex-direction: column;
