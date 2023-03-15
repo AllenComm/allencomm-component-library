f92d3ecb6c827e764e1be79355758dc38e00161b
GeorgeZack <georgez@allencomm.com>
2023-03-15 11:22:59 -0500

    Card styling

--- src/card.js
+++ src/card.js
@@ -7,13 +7,15 @@ export default class Card extends HTMLElement {
 				* {
 					box-sizing: border-box;
 				}
-				div {
+				:host {
 					background-color: #fff;
 					border-radius: 3px;
 					box-shadow: 0 4px 5px 0 rgb(0 0 0 / 14%), 0 1px 10px 0 rgb(0 0 0 / 12%), 0 2px 4px -1px rgb(0 0 0 / 30%);
+					display: block;
+				}
+				div {
 					display: flex;
 					flex-direction: column;
-					gap: 10px;
 					width: 100%;
 				}
 			</style>
