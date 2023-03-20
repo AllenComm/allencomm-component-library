c87656c54284391265e4647e180e7c652e9e12a0
GeorgeZack <georgez@allencomm.com>
2023-03-20 13:00:05 -0500

    Card density/shadows fixed

--- src/card.js
+++ src/card.js
@@ -10,25 +10,27 @@ export default class Card extends HTMLElement {
 				:host {
 					background-color: #fff;
 					border-radius: 3px;
-					/*box-shadow: 0 4px 5px 0 rgb(0 0 0 / 14%), 0 1px 10px 0 rgb(0 0 0 / 12%), 0 2px 4px -1px rgb(0 0 0 / 30%);*/
 					box-shadow: 0 2px 1px -1px rgb(0 0 0 / 20%), 0 1px 1px 0 rgb(0 0 0 / 14%), 0 1px 3px 0 rgb(0 0 0 / 12%);
 					display: block;
-					margin: 10px;
 					padding: 15px;
 				}
 				:host([density='comfortable']) {
-					margin: 5px;
 					padding: 10px;
 				}
 				:host([density='comfortable']) div {
 					gap: 6px;
 				}
 				:host([density='compact']) {
-					margin: 2px;
 					padding: 5px;
 				}
 				:host([density='compact']) div {
 					gap: 2px;
+				}
+				:host([density='none']) {
+					padding: 0;
+				}
+				:host([density='none']) div {
+					gap: 0;
 				}
 				:host([shadow='0']) {
 					box-shadow: none;
