63d6c3173489a39da6f4dd7c72d8bf0dfe6e387f
Scott Zackrison <cszackrison@gmail.com>
2023-03-22 18:47:11 -0600

    minor style fix

--- src/card.js
+++ src/card.js
@@ -41,6 +41,7 @@ export default class Card extends HTMLElement {
 					box-shadow: 0 3px 3px -2px rgb(0 0 0 / 20%), 0 3px 4px 0 rgb(0 0 0 / 14%), 0 1px 8px 0 rgb(0 0 0 / 12%);
 				}
 				div {
+					contain: content;
 					display: flex;
 					flex-direction: column;
 					width: 100%;
