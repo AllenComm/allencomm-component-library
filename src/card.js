4705c7aec6f3ed00c598665cefda4ac6345724e6
Scott Zackrison <cszackrison@gmail.com>
2023-03-22 18:51:15 -0600

    updated shadows

--- src/card.js
+++ src/card.js
@@ -10,7 +10,7 @@ export default class Card extends HTMLElement {
 				:host {
 					background-color: #fff;
 					border-radius: 3px;
-					box-shadow: 0 2px 1px -1px rgb(0 0 0 / 20%), 0 1px 1px 0 rgb(0 0 0 / 14%), 0 1px 3px 0 rgb(0 0 0 / 12%);
+					box-shadow: 0 2px 2px 0 rgb(0 0 0 / 14%), 0 3px 1px -2px rgb(0 0 0 / 12%), 0 1px 5px 0 rgb(0 0 0 / 20%);
 					display: block;
 				}
 				:host([density='cozy']) {
@@ -35,10 +35,16 @@ export default class Card extends HTMLElement {
 					box-shadow: none;
 				}
 				:host([shadow='2']) {
-					box-shadow: 0 3px 1px -2px rgb(0 0 0 / 20%), 0 2px 2px 0 rgb(0 0 0 / 14%), 0 1px 5px 0 rgb(0 0 0 / 12%);
+					box-shadow: 0 4px 5px 0 rgb(0 0 0 / 14%), 0 1px 10px 0 rgb(0 0 0 / 12%), 0 2px 4px -1px rgb(0 0 0 / 30%);
 				}
 				:host([shadow='3']) {
-					box-shadow: 0 3px 3px -2px rgb(0 0 0 / 20%), 0 3px 4px 0 rgb(0 0 0 / 14%), 0 1px 8px 0 rgb(0 0 0 / 12%);
+					box-shadow: 0 8px 17px 2px rgb(0 0 0 / 14%), 0 3px 14px 2px rgb(0 0 0 / 12%), 0 5px 5px -3px rgb(0 0 0 / 20%);
+				}
+				:host([shadow='4']) {
+					box-shadow: 0 16px 24px 2px rgb(0 0 0 / 14%), 0 6px 30px 5px rgb(0 0 0 / 12%), 0 8px 10px -7px rgb(0 0 0 / 20%);
+				}
+				:host([shadow='5']) {
+					box-shadow: 0 24px 38px 3px rgb(0 0 0 / 14%), 0 9px 46px 8px rgb(0 0 0 / 12%), 0 11px 15px -7px rgb(0 0 0 / 20%);
 				}
 				div {
 					contain: content;
