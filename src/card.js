7adce2c0e07eec593ecaca398d8e511e3c9d2b6c
Scott Zackrison <cszackrison@gmail.com>
2023-03-22 19:23:15 -0600

    another fix

--- src/card.js
+++ src/card.js
@@ -13,7 +13,6 @@ export default class Card extends HTMLElement {
 					box-shadow: 0 2px 2px 0 rgb(0 0 0 / 14%), 0 3px 1px -2px rgb(0 0 0 / 12%), 0 1px 5px 0 rgb(0 0 0 / 20%);
 					display: block;
 					contain: content;
-					height: 100%;
 					width: 100%;
 				}
 				:host([density='cozy']) {
