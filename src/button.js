21330d75647f175be6d483089de2c959f0db6a3e
BennettDISH <43225774+BennettDISH@users.noreply.github.com>
2023-06-22 09:18:23 -0500

    Adding button styles

--- src/button.js
+++ src/button.js
@@ -14,12 +14,21 @@ export default class Button extends HTMLElement {
 				}
 				button {
 					align-items: flex-start;
+					background-color: #D46027;
+					border: none;
+					border-radius: 5px;
 					cursor: pointer;
+					color: white;
 					display: flex;
+					padding: 10px 25px;
+					text-transform: uppercase;
+				}
+				button:hover {
+					background-color: #FC6E28;
 				}
 				button[disabled] {
+					background-color: #EEEEEE;
 					cursor: default;
-					pointer-events: none;
 				}
 			</style>
 			<button tabindex='-1'>
@@ -31,9 +40,9 @@ export default class Button extends HTMLElement {
 	}
 
 	get #button() { return this.shadowRoot.querySelector('button'); }
+	get #disabled() { return this._disabled; }
 
-	get disabled() { return this._disabled; }
-	set disabled(newVal) {
+	set #disabled(newVal) {
 		const bool = newVal === 'true' || newVal === true;
 		this._disabled = bool;
 		if (bool) {
@@ -52,18 +61,15 @@ export default class Button extends HTMLElement {
 	attributeChangedCallback(attr, oldVal, newVal) {
 		if (attr === 'disabled') {
 			const bool = newVal === 'true' || newVal === true;
-			this.disabled = bool;
+			this.#disabled = bool;
 		}
 	}
 
 	connectedCallback() {
 		if (this.getAttribute('disabled') === 'true') {
-			this.disabled = true;
+			this.#disabled = true;
 		} else {
-			this.disabled = false;
-		}
-		if (this.getAttribute('style') != null) {
-			this.#button.setAttribute('style', this.getAttribute('style'));
+			this.#disabled = false;
 		}
 	}
 }
