31d80b58b4b1cd6e9a63dc1da3c862d515965e28
GeorgeZack <georgez@allencomm.com>
2023-10-23 16:19:48 -0500

    Fixes

--- src/password.js
+++ src/password.js
@@ -50,7 +50,7 @@ export default class Password extends HTMLElement {
 					width: 30px;
 					z-index: 2;
 				}
-				.icon div, ::slotted(*[slot='toggle-show']), ::slotted(*[slot='toggle-hide']) {
+				.icon button, ::slotted(*[slot='toggle-show']), ::slotted(*[slot='toggle-hide']) {
 					display: flex !important;
 					height: 100%;
 					max-height: 22px !important;
@@ -67,20 +67,20 @@ export default class Password extends HTMLElement {
 			<label tabindex='-1'>
 				<slot></slot>
 				<input type='password' autocomplete='current-password' id='current-password' name='password'/>
-				<div class='icon toggle-show'>
+				<button class='icon toggle-show'>
 					<div>
 						<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
 							<path d="M480-320q75 0 127.5-52.5T660-500q0-75-52.5-127.5T480-680q-75 0-127.5 52.5T300-500q0 75 52.5 127.5T480-320Zm0-72q-45 0-76.5-31.5T372-500q0-45 31.5-76.5T480-608q45 0 76.5 31.5T588-500q0 45-31.5 76.5T480-392Zm0 192q-146 0-266-81.5T40-500q54-137 174-218.5T480-800q146 0 266 81.5T920-500q-54 137-174 218.5T480-200Zm0-300Zm0 220q113 0 207.5-59.5T832-500q-50-101-144.5-160.5T480-720q-113 0-207.5 59.5T128-500q50 101 144.5 160.5T480-280Z"/>
 						</svg>
 					</div>
-				</div>
-				<div class='icon toggle-hide hidden'>
+				</button>
+				<button class='icon toggle-hide hidden'>
 					<div>
 						<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
 							<path xmlns="http://www.w3.org/2000/svg" d="m644-428-58-58q9-47-27-88t-93-32l-58-58q17-8 34.5-12t37.5-4q75 0 127.5 52.5T660-500q0 20-4 37.5T644-428Zm128 126-58-56q38-29 67.5-63.5T832-500q-50-101-143.5-160.5T480-720q-29 0-57 4t-55 12l-62-62q41-17 84-25.5t90-8.5q151 0 269 83.5T920-500q-23 59-60.5 109.5T772-302Zm20 246L624-222q-35 11-70.5 16.5T480-200q-151 0-269-83.5T40-500q21-53 53-98.5t73-81.5L56-792l56-56 736 736-56 56ZM222-624q-29 26-53 57t-41 67q50 101 143.5 160.5T480-280q20 0 39-2.5t39-5.5l-36-38q-11 3-21 4.5t-21 1.5q-75 0-127.5-52.5T300-500q0-11 1.5-21t4.5-21l-84-82Zm319 93Zm-151 75Z"/>
 						</svg>
 					</div>
-				</div>
+				</button>
 				<slot name='toggle-show'></slot>
 				<slot name='toggle-hide'></slot>
 			</label>
@@ -103,15 +103,35 @@ export default class Password extends HTMLElement {
 		if (bool) {
 			this.input.removeEventListener('input', this.handleChange);
 			this.input.setAttribute('disabled', bool);
-			if (this.#slotToggleShow) this.#slotToggleShow.removeEventListener('click', this.handlePasswordToggle);
-			if (this.#slotToggleHide) this.#slotToggleHide.removeEventListener('click', this.handlePasswordToggle);
+			if (this.#slotToggleShow) {
+				this.#slotToggleShow.removeEventListener('click', this.handlePasswordToggle);
+				this.#slotToggleShow.setAttribute('aria-disabled', bool);
+				this.#slotToggleShow.setAttribute('aria-hidden', bool);
+				this.#slotToggleShow.removeAttribute('aria-label');
+			}
+			if (this.#slotToggleHide) {
+				this.#slotToggleHide.removeEventListener('click', this.handlePasswordToggle);
+				this.#slotToggleHide.setAttribute('aria-disabled', bool);
+				this.#slotToggleHide.setAttribute('aria-hidden', bool);
+				this.#slotToggleHide.removeAttribute('aria-label');
+			}
 			this.setAttribute('aria-disabled', bool);
 			this.setAttribute('aria-hidden', bool);
 		} else {
 			this.input.addEventListener('input', this.handleChange);
 			this.input.removeAttribute('disabled');
-			if (this.#slotToggleShow) this.#slotToggleShow.addEventListener('click', this.handlePasswordToggle);
-			if (this.#slotToggleHide) this.#slotToggleHide.addEventListener('click', this.handlePasswordToggle);
+			if (this.#slotToggleShow) {
+				this.#slotToggleShow.addEventListener('click', this.handlePasswordToggle);
+				this.#slotToggleShow.removeAttribute('aria-disabled');
+				this.#slotToggleShow.removeAttribute('aria-hidden');
+				this.#slotToggleShow.setAttribute('aria-label', 'Hide password.');
+			}
+			if (this.#slotToggleHide) {
+				this.#slotToggleHide.addEventListener('click', this.handlePasswordToggle);
+				this.#slotToggleHide.removeAttribute('aria-disabled');
+				this.#slotToggleHide.removeAttribute('aria-hidden');
+				this.#slotToggleHide.setAttribute('aria-label', 'Show password as plain text. Warning: this will display your password on the screen.');
+			}
 			this.removeAttribute('aria-disabled');
 			this.removeAttribute('aria-hidden');
 		}
@@ -163,6 +183,7 @@ export default class Password extends HTMLElement {
 		if (!this.#slotToggleHide) {
 			this.shadowRoot.querySelector('[name=toggle-hide]').classList.add('hidden');
 			this.#slotToggleHide = this.shadowRoot.querySelector('.toggle-hide');
+			this.#slotToggleHide.setAttribute('aria-hidden', true);
 		}
 		if (this.getAttribute('disabled') === 'true') {
 			this.disabled = true;
@@ -179,11 +200,15 @@ export default class Password extends HTMLElement {
 		if (this.input.type === 'password') {
 			this.input.type = 'text';
 			this.#slotToggleShow.classList.add('hidden');
+			this.#slotToggleShow.setAttribute('aria-hidden', true);
 			this.#slotToggleHide.classList.remove('hidden');
+			this.#slotToggleHide.setAttribute('aria-hidden', false);
 		} else {
 			this.input.type = 'password';
 			this.#slotToggleShow.classList.remove('hidden');
+			this.#slotToggleShow.setAttribute('aria-hidden', false);
 			this.#slotToggleHide.classList.add('hidden');
+			this.#slotToggleHide.setAttribute('aria-hidden', true);
 		}
 	}
 }
